c3e1279988b568169464f7fcbcd388c399f21ed4
Subin Jang <wkdtnqls0506@naver.com>
2023-11-21 23:40:22 +0900

    design: 공통으로 쓰일 theme 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Global } from '@emotion/react';
 import Header from './components/common/Header';
-import globalStyles from './style/globalStyles';
 
 function App() {
   return (
     <div>
-      <Global styles={globalStyles} />
       <Header />
       <Outlet />
     </div>
