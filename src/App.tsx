55b5707626ead7748dc9beb6df77760517f0f6dc
TaePoong719 <just5538@gmail.com>
2023-11-21 15:13:30 +0900

    Chore : 1차 스프린트 API 모킹

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { axiosWithNoToken } from './Axios';
+import { axiosWithAccessToken, axiosWithNoToken } from './Axios';
 
 type RequestSignup = {
   name: string;
   email: string;
   password: string;
   passwordConfirm: string;
+};
+
+type RequestSignin = {
+  email: string;
+  password: string;
+};
+
+type RequestMembers = {
+  password?: string;
+  passwordConfirm?: string;
+  photoURL?: string;
+};
+
+type RequestOrders = {
+  memberId: string;
+  productId: string;
+  checkInTime: string;
+  checkOutTime: string;
+  payMethod: string;
+  totalAmount: number;
 };
 
 function App() {
@@ -25,7 +45,52 @@ function App() {
         password,
         passwordConfirm,
       });
-      console.log(data);
+      console.log('Signup', data);
+    };
+
+    const fetchDataSignin = async ({ email, password }: RequestSignin) => {
+      const data = await axiosWithNoToken.post('/api/signin', {
+        email,
+        password,
+      });
+      console.log('Signin', data);
+    };
+
+    const fetchDataRefresh = async () => {
+      const data = await axiosWithNoToken.post('/api/refresh');
+      console.log('refresh', data);
+    };
+
+    const fetchDataMembers = async ({
+      password,
+      passwordConfirm,
+      photoURL,
+    }: RequestMembers) => {
+      const data = await axiosWithAccessToken.patch('/api/members', {
+        password,
+        passwordConfirm,
+        photoURL,
+      });
+      console.log('members', data);
+    };
+
+    const fetchDataOrders = async ({
+      memberId,
+      productId,
+      checkInTime,
+      checkOutTime,
+      payMethod,
+      totalAmount,
+    }: RequestOrders) => {
+      const data = await axiosWithAccessToken.post('/api/orders', {
+        memberId,
+        productId,
+        checkInTime,
+        checkOutTime,
+        payMethod,
+        totalAmount,
+      });
+      console.log('orders', data);
     };
 
     fetchDataSignup({
@@ -33,6 +98,25 @@ function App() {
       email: 'abc@gmail.com',
       password: 'abcabcabc',
       passwordConfirm: 'abcabcabc',
+    });
+    fetchDataSignin({
+      email: 'abc@gmail.com',
+      password: 'abcabcabc',
+    });
+    fetchDataRefresh();
+    fetchDataMembers({
+      photoURL: '',
+      password: 'abcabcabc',
+      passwordConfirm: 'abcabcabc',
+    });
+    // API 명세에 checkInTime이 LocalDateTime이라 돼있는데 확실하지 않아 일단 string 타입으로 넣어놨습니다 추후 백엔드분과 얘기가 필요해 보입니다
+    fetchDataOrders({
+      memberId: '123',
+      productId: '456',
+      checkInTime: '2023-11-20 13:00:00',
+      checkOutTime: '2023-11-22 12:00:00',
+      payMethod: 'KAKAO_PAY',
+      totalAmount: 95000,
     });
   }, []);
 
--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -8,11 +8,6 @@ async function sleep(timeout: number) {
 }
 
 const handlers = [
-  rest.get('/api/people', async (_, res, ctx) => {
-    await sleep(200);
-
-    return res(ctx.status(200), ctx.json('its okay'));
-  }),
   rest.post('/api/signup', async (_, res, ctx) => {
     await sleep(500);
     return res(
@@ -25,6 +20,72 @@ const handlers = [
           name: '최우혁',
           photoURL:
             'https://firebasestorage.googleapis.com/v0/b/employee-management-c0a21.appspot.com/o/bigimage%2F%EA%B0%80%EB%A0%8C.jpg?alt=media&token=f5dd05f5-1036-44d3-9787-6abe2a42cc90',
+        },
+      }),
+    );
+  }),
+  rest.post('/api/signin', async (_, res, ctx) => {
+    await sleep(500);
+    return res(
+      ctx.status(200),
+      ctx.json({
+        code: 200,
+        message: '성공적으로 로그인 했습니다.',
+        data: {
+          accessToken: import.meta.env.VITE_ACCESS_TOKEN,
+          refreshToken: import.meta.env.VITE_ACCESS_TOKEN,
+          email: 'abc@gmail.com',
+          name: '최우혁',
+          photoUrl:
+            'https://firebasestorage.googleapis.com/v0/b/employee-management-c0a21.appspot.com/o/bigimage%2F%EA%B0%80%EB%A0%8C.jpg?alt=media&token=f5dd05f5-1036-44d3-9787-6abe2a42cc90',
+        },
+      }),
+    );
+  }),
+  rest.post('/api/refresh', async (_, res, ctx) => {
+    await sleep(500);
+    return res(
+      ctx.status(200),
+      ctx.json({
+        code: 200,
+        message: '성공적으로 토큰을 발급했습니다.',
+        data: {
+          accessToken: import.meta.env.VITE_ACCESS_TOKEN,
+          refreshToken: import.meta.env.VITE_ACCESS_TOKEN,
+          email: 'abc@gmail.com',
+          name: '최우혁',
+          photoUrl:
+            'https://firebasestorage.googleapis.com/v0/b/employee-management-c0a21.appspot.com/o/bigimage%2F%EA%B0%80%EB%A0%8C.jpg?alt=media&token=f5dd05f5-1036-44d3-9787-6abe2a42cc90',
+        },
+      }),
+    );
+  }),
+  rest.patch('/api/members', async (_, res, ctx) => {
+    await sleep(500);
+    return res(
+      ctx.status(200),
+      ctx.json({
+        code: 200,
+        message: '성공적으로 회원 정보를 수정했습니다.',
+        data: {
+          memberId: 1,
+          email: 'abc@gmail.com',
+          name: '최우혁',
+          photoUrl:
+            'https://firebasestorage.googleapis.com/v0/b/employee-management-c0a21.appspot.com/o/bigimage%2F%EA%B0%80%EB%A0%8C.jpg?alt=media&token=f5dd05f5-1036-44d3-9787-6abe2a42cc90',
+        },
+      }),
+    );
+  }),
+  rest.post('/api/orders', async (_, res, ctx) => {
+    await sleep(500);
+    return res(
+      ctx.status(200),
+      ctx.json({
+        code: 201,
+        message: '성공적으로 예약이 완료되었습니다.',
+        data: {
+          orderToken: 'orderToken-1234',
         },
       }),
     );
