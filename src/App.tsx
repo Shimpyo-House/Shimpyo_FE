7be8137ac9bcc3b297e75aa518557b89fd7ac7a9
Subin Jang <wkdtnqls0506@naver.com>
2023-11-21 23:26:35 +0900

    Design: globalStyles 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Global } from '@emotion/react';
 import Header from './components/common/Header';
+import globalStyles from './style/globalStyles';
 
 function App() {
   return (
     <div>
+      <Global styles={globalStyles} />
       <Header />
       <Outlet />
     </div>
