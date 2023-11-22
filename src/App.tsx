c74a5b082f2c42bf6287d880eff704da44a66f35
jiohjung98 <ckaquz98@naver.com>
2023-11-22 14:49:42 +0900

    Feat: 상품 상세 페이지 상품정보 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
+// // App.tsx
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Header from './components/common/Header';
+// import ProductDetail from './components/layout/productsDetail/ProductDetail';
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <Header />
+//         <Routes>
+//           <Route path="/product-detail" element={<ProductDetail />} />
+//           {/* 다른 경로에 대한 처리 */}
+//         </Routes>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/common/Header';
+import ProductsDetail from './components/layout/productsDetail/ProductsDetail';
 
 function App() {
   return (
     <div>
       <Header />
+      <ProductsDetail />
       <Outlet />
     </div>
   );
