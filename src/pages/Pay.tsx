aa0e7ff0c068628808dc66740723eaed687ee87e
seacrab808 <seacrab808@naver.com>
2023-11-23 13:35:49 +0900

    Design: 예약자/사용자 정보 입력 디자인

--- src/pages/Pay.tsx
+++ src/pages/Pay.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import CheckBooking from '../components/layout/Pay/CheckBooking';
+import BookingUser from '../components/layout/Pay/BookingUser';
 
 const Pay = () => {
-  return <CheckBooking />;
+  return (
+    <>
+      <CheckBooking />
+      <BookingUser />
+    </>
+  );
 };
 
 export default Pay;
