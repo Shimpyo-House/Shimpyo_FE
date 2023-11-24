7720a9be30ea57db7a80b09ff33db4d54f154205
Subin Jang <wkdtnqls0506@naver.com>
2023-11-24 18:25:04 +0900

    feat: 장바구니 체크박스 로직

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,9 +1,7 @@
 /* eslint-disable import/no-cycle */
 import { useEffect, useState } from 'react';
-import { css } from '@emotion/react';
 import { ResponseCartsData } from '../types';
 import cartAxios from '../api/cart';
-import theme from '../style/theme';
 import CartItem from '../components/layout/cart/CartItem';
 import CartTotal from '../components/layout/cart/CartTotal';
 
@@ -25,10 +23,6 @@ const Cart = () => {
 
   return (
     <>
-      <label htmlFor="box" css={Label}>
-        <input id="box" type="checkbox" css={CheckBox} />
-        <p css={AllSelect}>전체 선택</p>
-      </label>
       <CartItem carts={carts} />
       <CartTotal />
     </>
@@ -36,36 +30,3 @@ const Cart = () => {
 };
 
 export default Cart;
-
-const Label = css`
-  display: flex;
-  align-items: center;
-  gap: 0.8rem;
-
-  margin: 1rem;
-`;
-
-export const CheckBox = css`
-  width: 1.5rem;
-  height: 1.5rem;
-
-  border: 1.5px solid ${theme.colors.gray600};
-  border-radius: 0.35rem;
-
-  cursor: pointer;
-  appearance: none;
-
-  &:checked {
-    border-color: transparent;
-
-    background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 16 16' fill='white' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M5.707 7.293a1 1 0 0 0-1.414 1.414l2 2a1 1 0 0 0 1.414 0l4-4a1 1 0 0 0-1.414-1.414L7 8.586 5.707 7.293z'/%3e%3c/svg%3e");
-    background-size: 100% 100%;
-    background-position: 50%;
-    background-repeat: no-repeat;
-    background-color: ${theme.colors.blue700};
-  }
-`;
-
-const AllSelect = css`
-  font-weight: 700;
-`;
