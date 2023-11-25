f1b83fc754a180a7859969850b5aebe05dd1fbce
jiohjung98 <ckaquz98@naver.com>
2023-11-25 19:09:20 +0900

    Feat: date range 설정

--- src/components/layout/productsDetail/Calendar.tsx
+++ src/components/layout/productsDetail/Calendar.tsx
@@ -1,60 +1,55 @@
+/* eslint-disable react/no-unused-state */
 /* eslint-disable import/no-extraneous-dependencies */
-// /* eslint-disable @typescript-eslint/no-use-before-define */
-// /* eslint-disable @typescript-eslint/no-unused-vars */
-// /* eslint-disable import/no-extraneous-dependencies */
-// // import * as React from 'react';
-// import dayjs from 'dayjs';
-// import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
-// // import { MobileDateRangePicker } from '@mui/x-date-pickers-pro/MobileDateRangePicker';
-// // import { DesktopDateRangePicker } from '@mui/x-date-pickers-pro/DesktopDateRangePicker';
-// // import { StaticDateRangePicker } from '@mui/x-date-pickers-pro/StaticDateRangePicker';
-// // import { pickersLayoutClasses } from '@mui/x-date-pickers/PickersLayout';
-// import { css } from '@emotion/react';
-
-// export default function Calendar() {
-//   // const [value, setValue] = React.useState<Day | null>(null);
-//   const today = dayjs();
-//   const tomorrow = today.add(1, 'day');
-
-//   return (
-//     <div css={DayCalendar}>
-//       <LocalizationProvider dateAdapter={AdapterDayjs}>
-//         <DemoContainer
-//           components={['DateRangePicker', 'StaticDateRangePicker']}
-//         >
-//           <DemoItem label="날짜 선택" component="DateRangePicker">
-//             <DateRangePicker defaultValue={[today, tomorrow]} />
-//           </DemoItem>
-//         </DemoContainer>
-//       </LocalizationProvider>
-//     </div>
-//   );
-// }
-
-// const DayCalendar = css`
-//   width: 100%;
-//   //   max-width: 1000px;
-
-//   display: flex;
-//   justify-content: flex-start;
-
-//   margin-top: 2.5rem;
-// `;
-
 import { Component } from 'react';
-import { Calendar } from 'react-date-range';
+import { DateRange } from 'react-date-range';
 
-class CalendarComponent extends Component {
-  static handleSelect = (date: any) => {
-    console.log(date);
+interface CalendarState {
+  startDate: Date;
+  endDate: Date;
+  key: string;
+}
+
+class CalendarComponent extends Component<{}, CalendarState> {
+  constructor(props: any) {
+    super(props);
+    const today = new Date();
+    this.state = {
+      startDate: today,
+      endDate: new Date(today.getTime() + 24 * 60 * 60 * 1000),
+      key: 'selection',
+    };
+  }
+
+  onRangeChange = (ranges: any) => {
+    const { selection } = ranges;
+    this.setState({
+      startDate: selection.startDate,
+      endDate: selection.endDate,
+      key: selection.key,
+    });
   };
 
   render() {
+    const { startDate, endDate } = this.state;
+    const ranges = {
+      startDate,
+      endDate,
+      key: 'selection',
+    };
+
     return (
-      <Calendar date={new Date()} onChange={CalendarComponent.handleSelect} />
+      <div>
+        <DateRange
+          editableDateInputs
+          onChange={this.onRangeChange}
+          moveRangeOnFirstSelection={false}
+          ranges={[ranges]}
+        />
+        <br />
+        <div>Start Date : {startDate.toString()}</div>
+        <br />
+        <div>End Date : {endDate.toString()}</div>
+      </div>
     );
   }
 }
