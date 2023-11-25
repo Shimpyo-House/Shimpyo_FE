701e45d7672bb24c65fc40b74c3faaa89a9c0022
Subin Jang <wkdtnqls0506@naver.com>
2023-11-25 23:28:40 +0900

    feat: 장바구니 계산 로직

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { ResponseCartsData } from '../types';
 import cartAxios from '../api/cart';
 import CartItem from '../components/layout/cart/CartItem';
-import CartTotal from '../components/layout/cart/CartTotal';
 
 const Cart = () => {
   const [carts, setCarts] = useState<ResponseCartsData[] | null>(null);
@@ -17,16 +16,10 @@ const Cart = () => {
         console.error('장바구니 error: ', error);
       }
     };
-
     cartData();
   }, []);
 
-  return (
-    <>
-      <CartItem carts={carts} />
-      <CartTotal />
-    </>
-  );
+  return <CartItem carts={carts} />;
 };
 
 export default Cart;
