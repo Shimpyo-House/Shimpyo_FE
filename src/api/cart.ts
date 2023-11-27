09f35a18b42f0e818cbcf52707a68dc6a5773d0f
Subin Jang <wkdtnqls0506@naver.com>
2023-11-28 02:19:45 +0900

    feat: 장바구니 품절 여부 확인을 위한 post & 장바구니 아이템 삭제 axios

--- src/api/cart.ts
+++ src/api/cart.ts
@@ -1,7 +1,7 @@
 /* eslint-disable consistent-return */
 import axios from 'axios';
 
-const cartAxios = async () => {
+const cartGetAxios = async () => {
   try {
     const response = await axios.get('/api/carts');
     return response.data.data;
@@ -10,4 +10,30 @@ const cartAxios = async () => {
   }
 };
 
-export default cartAxios;
+interface PostRoomData {
+  roomId: number;
+  startDate: string;
+  endDate: string;
+}
+
+const cartPostToPay = async (roomData: PostRoomData[]) => {
+  try {
+    const response = await axios.post('/api/carts', {
+      roomData,
+    });
+    return response.data.data;
+  } catch (err) {
+    alert('⚠️ 장바구니 post 에러');
+  }
+};
+
+const cartDeleteItem = async (cartId: number) => {
+  try {
+    const response = await axios.delete(`/api/carts/${cartId}`);
+    return response.data;
+  } catch (err) {
+    alert('⚠️ 장바구니 삭제 에러');
+  }
+};
+
+export { cartGetAxios, cartPostToPay, cartDeleteItem };
