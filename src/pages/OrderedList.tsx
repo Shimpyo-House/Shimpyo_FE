52aba34163de5b3d1ae5a98acd353d6609a395ec
seacrab808 <seacrab808@naver.com>
2023-11-28 00:47:51 +0900

    Feat: 결제 페이지 로딩 추가

--- src/pages/OrderedList.tsx
+++ src/pages/OrderedList.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import { css } from '@emotion/react';
 import OrderAxios from '../api/OrderComplete';
 import OrderedProduct from '../components/layout/Pay/OrderedProduct';
+import theme from '../style/theme';
 
 const OrderedList = () => {
   const [orderCom, setOrderCom] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const orderedData = async () => {
@@ -19,6 +21,14 @@ const OrderedList = () => {
     orderedData();
   }, []);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const orderComplete = async () => {
     try {
       const data = await OrderAxios();
@@ -31,28 +41,104 @@ const OrderedList = () => {
   orderComplete();
 
   return (
-    <nav>
-      <div css={OrderComplete}>{orderCom}</div>
-      <OrderedProduct />
-      <OrderedProduct />
+    <div>
+      {loading ? (
+        <div css={LoadingWrap}>
+          <div css={Loader}>
+            <li className="ball"> </li>
+            <li className="ball"> </li>
+            <li className="ball"> </li>
+          </div>
+          <div css={LoadingMessage}>결제가 진행중입니다</div>
+        </div>
+      ) : (
+        <nav>
+          <div css={OrderComplete}>{orderCom}</div>
+          <OrderedProduct />
+          <OrderedProduct />
 
-      <div css={OrderedWrap}>
-        <div css={OrderedWrapEl}>
-          <h3>이용자 정보</h3>
-          <div>소유나 (010-0000-0000)</div>
-        </div>
-        <div css={OrderedWrapEl}>
-          <h3>결제 수단</h3>
-          <div>토스페이</div>
-        </div>
-        <div css={OrderedWrapEl}>
-          <h3>총 결제 금액</h3>
-          <div>156,000원</div>
-        </div>
-      </div>
-    </nav>
+          <div css={OrderedWrap}>
+            <div css={OrderedWrapEl}>
+              <h3>이용자 정보</h3>
+              <div>소유나 (010-0000-0000)</div>
+            </div>
+            <div css={OrderedWrapEl}>
+              <h3>결제 수단</h3>
+              <div>토스페이</div>
+            </div>
+            <div css={OrderedWrapEl}>
+              <h3>총 결제 금액</h3>
+              <div>156,000원</div>
+            </div>
+          </div>
+        </nav>
+      )}
+    </div>
   );
 };
+
+const LoadingWrap = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  height: 75vh;
+`;
+
+const Loader = css`
+  width: 12.5rem;
+  display: flex;
+  justify-content: space-evenly;
+
+  .ball {
+    list-style: none;
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    background-color: #fff;
+  }
+
+  .ball:nth-child(1) {
+    animation: bounce-1 1.4s ease-in-out infinite;
+  }
+
+  @keyframes bounce-1 {
+    50% {
+      transform: translateY(-1rem);
+      background-color: ${theme.colors.blue700};
+    }
+  }
+
+  .ball:nth-child(2) {
+    animation: bounce-3 1.4s ease-in-out 0.2s infinite;
+  }
+
+  @keyframes bounce-2 {
+    50% {
+      transform: translateY(-1rem);
+      background-color: ${theme.colors.blue700};
+    }
+  }
+
+  .ball:nth-child(3) {
+    animation: bounce-3 1.4s ease-in-out 0.4s infinite;
+  }
+
+  @keyframes bounce-3 {
+    50% {
+      transform: translateY(-1rem);
+      background-color: ${theme.colors.blue700};
+    }
+  }
+`;
+
+const LoadingMessage = css`
+  margin-top: 6rem;
+  font-size: 1.5rem;
+  font-weight: 900;
+  color: ${theme.colors.blue700};
+`;
 
 const OrderComplete = css`
   margin-top: 4rem;
