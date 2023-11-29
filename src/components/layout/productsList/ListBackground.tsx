4c0f208c2414aa7493c4ce5271c2a4ec39a79609
Yamyam-code <rnffjt@gmail.com>
2023-11-29 17:03:49 +0900

    Design: 배경 파도 생성

--- src/components/layout/productsList/ListBackground.tsx
+++ src/components/layout/productsList/ListBackground.tsx
@@ -1,8 +1,14 @@
-import { css } from '@emotion/react';
+import { css, keyframes } from '@emotion/react';
 import theme from '../../../style/theme';
 
 const ListBackground = () => {
-  return <div css={Background} />;
+  return (
+    <div css={Background}>
+      <div css={ThirdWave} />
+      <div css={SecondWave} />
+      <div css={FirstWave} />
+    </div>
+  );
 };
 
 export default ListBackground;
@@ -15,5 +21,56 @@ const Background = css`
   width: 100vw;
   height: 100vh;
 
-  background-color: ${theme.colors.blue100};
+  background-color: ${theme.colors.blue600};
+`;
+
+const WaveMove = keyframes`
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const FirstWave = css`
+  width: 280vw;
+  height: 280vw;
+
+  position: absolute;
+  top: -430%;
+  left: -90%;
+
+  border-radius: 48%;
+
+  background-color: ${theme.colors.blue200};
+
+  animation: ${WaveMove} 17s infinite linear;
+`;
+
+const SecondWave = css`
+  width: 280vw;
+  height: 280vw;
+
+  position: absolute;
+  top: -427%;
+  left: -90%;
+
+  border-radius: 48%;
+
+  background-color: ${theme.colors.blue700};
+
+  animation: ${WaveMove} 18s infinite linear;
+`;
+
+const ThirdWave = css`
+  width: 280vw;
+  height: 280vw;
+
+  position: absolute;
+  top: -424%;
+  left: -90%;
+
+  border-radius: 48%;
+
+  background-color: ${theme.colors.blue800};
+
+  animation: ${WaveMove} 12s infinite linear;
 `;
