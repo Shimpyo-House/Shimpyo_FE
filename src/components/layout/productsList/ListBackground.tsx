0c6382c4aa03f597e07d37e0a9d83acd6a05eacc
Yamyam-code <rnffjt@gmail.com>
2023-11-29 18:13:06 +0900

    Design: 파도 위치 조정 및 필터 적용

--- src/components/layout/productsList/ListBackground.tsx
+++ src/components/layout/productsList/ListBackground.tsx
@@ -4,9 +4,10 @@ import theme from '../../../style/theme';
 const ListBackground = () => {
   return (
     <div css={Background}>
-      <div css={ThirdWave} />
       <div css={SecondWave} />
+      <div css={ThirdWave} />
       <div css={FirstWave} />
+      <div css={Blur} />
     </div>
   );
 };
@@ -35,7 +36,7 @@ const FirstWave = css`
   height: 280vw;
 
   position: absolute;
-  top: -430%;
+  bottom: 20%;
   left: -90%;
 
   border-radius: 48%;
@@ -50,14 +51,14 @@ const SecondWave = css`
   height: 280vw;
 
   position: absolute;
-  top: -427%;
+  bottom: 16%;
   left: -90%;
 
-  border-radius: 48%;
+  border-radius: 47%;
 
   background-color: ${theme.colors.blue700};
 
-  animation: ${WaveMove} 18s infinite linear;
+  animation: ${WaveMove} 12s infinite linear;
 `;
 
 const ThirdWave = css`
@@ -65,12 +66,23 @@ const ThirdWave = css`
   height: 280vw;
 
   position: absolute;
-  top: -424%;
+  bottom: 18%;
   left: -90%;
 
   border-radius: 48%;
 
   background-color: ${theme.colors.blue800};
 
-  animation: ${WaveMove} 12s infinite linear;
+  animation: ${WaveMove} 15s infinite linear;
+`;
+
+const Blur = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+  height: 100vh;
+
+  background-color: rgba(255, 2555, 255, 0.1);
 `;
