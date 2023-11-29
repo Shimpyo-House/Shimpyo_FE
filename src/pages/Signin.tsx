545c918831310c8ff063fe76cb69e18396ed3673
TaePoong719 <just5538@gmail.com>
2023-11-29 21:56:36 +0900

    Chore : index 파일 설정

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -37,7 +37,7 @@ export const SigninInnerContainer = css`
   width: 100%;
   max-width: 1080px;
 
-  margin: 5.5rem auto 0 auto;
+  margin: 7rem auto 0 auto;
 
   border-radius: 0.75rem;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
