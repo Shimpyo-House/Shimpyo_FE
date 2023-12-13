34928b2e91620a60f7da6f5fc28ad6f6b5358b78
jiohjung98 <ckaquz98@naver.com>
2023-12-13 17:39:51 +0900

    fix

--- src/components/layout/productsList/ColumnProduct.tsx
+++ src/components/layout/productsList/ColumnProduct.tsx
@@ -1,126 +1,41 @@
+/* eslint-disable @typescript-eslint/indent */
+
 import { css } from '@emotion/react';
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import theme from '../../../style/theme';
+import RowProduct from './RowProduct';
 import { ResponseProductsData } from '../../../types';
-import Star from '../../common/star';
+import SearchProduct from './SearchProduct';
 
 type PropsType = {
-  resData: ResponseProductsData;
+  data: ResponseProductsData[];
+  main: boolean;
 };
 
-const ColumnProduct = ({ resData }: PropsType) => {
-  const [price, setPrice] = useState(0);
-  const [address, setAddress] = useState('');
-
-  // 이후 훅 화 예정
-  useEffect(() => {
-    const result = resData.address.match(/^(\S+\s+\S+\s+\S+)\s/);
-    if (result) {
-      setAddress(result[1]);
-    }
-
-    if (resData.price === 0) {
-      setPrice(100000);
-    } else {
-      setPrice(resData.price);
-    }
-  }, []);
-
+const ColumnList = ({ data, main }: PropsType) => {
   return (
-    <Link to={`/products/${resData.productId}`}>
-      <div css={ProductBox}>
-        <img css={ProductImg} src={resData.image} alt="숙소 대표 사진" />
-        <p css={ProductName}>{resData.productName}</p>
-        <p css={ProductAddress}>{address}</p>
-        <div css={ProductScore}>
-          <div css={SpaceScore}>
-            <Star />
-            {resData.starAvg.toFixed(1)}
-          </div>
-        </div>
-        <p css={ProductPrice}>{price.toLocaleString()}원</p>
-      </div>
-    </Link>
+    <div css={ProductsBox}>
+      {data && main
+        ? data.map((e, i) => (
+            <RowProduct resData={e} rank={i + 1} key={e.productId} />
+          ))
+        : data.map((e, i) => (
+            <SearchProduct resData={e} rank={i + 1} key={e.productId} />
+          ))}
+    </div>
   );
 };
 
-export default ColumnProduct;
+export default ColumnList;
 
-const ProductBox = css`
-  width: 20.625rem;
-  height: 21.25rem;
+const ProductsBox = css`
+  width: 100%;
+
+  position: relative;
 
   display: flex;
-  flex-direction: column;
+  flex-wrap: wrap;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
+  align-content: space-between;
 
-  padding: 1.5625rem 1.25rem;
-
-  border: 1px solid ${theme.colors.gray300};
-  border-radius: 10px;
-
-  background-color: ${theme.colors.white};
-
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-
-  cursor: pointer;
-
-  transition: 0.2s all;
-  &:hover {
-    scale: 1.015;
-  }
-  &:active {
-    scale: 0.985;
-  }
-`;
-
-const ProductImg = css`
-  width: 100%;
-  height: 11.25rem;
-
-  background-color: ${theme.colors.gray700};
-
-  border-radius: 5px;
-`;
-
-const ProductName = css`
-  width: 100%;
-
-  display: flex;
-  justify-content: flex-start;
-
-  font-size: 1.25rem;
-  font-weight: 600;
-`;
-const ProductAddress = css`
-  width: 100%;
-  display: flex;
-  justify-content: flex-start;
-
-  font-size: 0.8rem;
-`;
-const ProductScore = css`
-  width: 100%;
-
-  display: flex;
-  justify-content: flex-start;
-
-  font-size: 0.875rem;
-`;
-const SpaceScore = css`
-  width: 2.4rem;
-
-  display: flex;
-  justify-content: space-between;
-`;
-const ProductPrice = css`
-  width: 100%;
-
-  display: flex;
-  justify-content: flex-end;
-
-  font-size: 1.35rem;
-  font-weight: 700;
+  gap: 3.125rem;
 `;
